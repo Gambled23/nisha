63d5856fdad6b4b0285ce29a743e6358c1204cc2
César Girón <ipog71@gmail.com>
2023-09-18 16:07:33 -0600

    rutas de producto/descripcions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\DescripcionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,10 +12,13 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the 'web' middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('productos', ProductoController::class);
+Route::resource('descripcions', ProductoController::class);
