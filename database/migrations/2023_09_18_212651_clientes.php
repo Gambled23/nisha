3ba1eae4afad082a6b173e0a37c2a8c58e8870e3
César González Girón <58153130+Gambled23@users.noreply.github.com>
2023-09-20 15:47:46 -0600

    Update 2023_09_18_212651_clientes.php

--- database/migrations/2023_09_18_212651_clientes.php
+++ database/migrations/2023_09_18_212651_clientes.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('nombre', 30);
             $table->string('correo', 30);
-            $table->integer('num_pedido');
-            $table->integer('telefono');
+            $table->string('telefono');
+            $table->string('direccion');
             $table->timestamps();
         });
     }
