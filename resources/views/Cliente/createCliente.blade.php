b3cdb35a75c039dd0f5d7c8c4bbadf14b8982fa7
Aranza Vanessa Perez Navarro <cybergoth2077@outlook.com>
2023-09-20 20:45:26 -0600

    create styles

--- resources/views/Cliente/createCliente.blade.php
+++ resources/views/Cliente/createCliente.blade.php
@@ -6,19 +6,29 @@
     @vite('resources/css/app.css')
     <title>Cliente</title>
 </head>
-<body>
-<h1>FORMULARIO DE CLIENTE</h1>
-    <form method="POST" action="{{ route('cliente.store') }}">
-        @csrf <!--cross site resource forgery-->
-        <label for="nombre">Nombre</label><br>
-        <input type="text" name="nombre" id="nombre"><br>
-        <label for="correo">Correo</label><br>
-        <input type="email" name="correo" id="correo" value="@gmail.com"><br>
-        <label for="telefono">Telefono</label><br>
-        <input type="text" name="telefono" id="telefono"><br>
-        <label for="direccion">Direccion</label><br>
-        <input type="text" name="direccion" id="direccion"><br><br>
-        <input type="submit" value="Enviar">
-    </form>
+<body class="flex justify-center">
+    <div class="w-full max-w-xs">
+    <h1 class="mb-10 text-2xl">FORMULARIO DE CLIENTE</h1>
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('cliente.store') }}">
+            @csrf <!--cross site resource forgery-->
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="nombre">Nombre</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Nombre" id="nombre">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="correo">Correo</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="@example.com" id="correo">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="telefono">Telefono</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Telefono" id="telefono">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="direccion">Direccion</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Dirección" id="direccion"><br><br>
+                <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Enviar</button>
+        </form>
+    </div>
+    
 </body>
 </html>
