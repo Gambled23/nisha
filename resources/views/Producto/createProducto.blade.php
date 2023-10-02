e2c4534d2a83451ac70e1caf2d3051c7bdd28d18
Aranza Vanessa Perez Navarro <cybergoth2077@outlook.com>
2023-10-02 15:27:18 -0600

    configuraciones jetstream, validaciones producto

--- resources/views/Producto/createProducto.blade.php
+++ resources/views/Producto/createProducto.blade.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Cliente</title>
+    <title>Producto</title>
 </head>
 <body class="flex justify-center">
     <div class="w-full max-w-xs">
         <h1 class="text-center mb-10 text-2xl font-mono"><br>FORMULARIO DE PRODUCTO</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+             </div>
+         @endif
+
             <form method="POST" action="{{ route('productos.store') }}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 @csrf <!--cross site resource forgery-->
                 <div class="mb-4">
@@ -17,11 +28,11 @@
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="informacion">Informacion</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="informacion" id="informacion" placeholder="Información">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="informacion" id="informacion" placeholder="Información" value="{{old('informacion')}}">
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="precio">Precio</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="precio" id="precio" placeholder="Precio">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="precio" id="precio" placeholder="Precio" value="{{old('precio')}}">
                 </div>
                 <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="disponible">Disponible</label>
